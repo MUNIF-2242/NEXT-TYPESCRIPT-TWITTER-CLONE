a31d5c19b467f96420005d09c372e7c6f88cc24d
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-28 10:47:02 +0600

    complete profile section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast'
-import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
-import Layout from '@/components/Layout'
-import LoginModal from '@/components/modals/LoginModal'
-import RegisterModal from '@/components/modals/RegisterModal'
-import '@/styles/globals.css'
+import Layout from '@/components/Layout';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
+import '@/styles/globals.css';
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
 
@@ -18,5 +20,5 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Layout>
     </SessionProvider>
-  )
+  );
 }
