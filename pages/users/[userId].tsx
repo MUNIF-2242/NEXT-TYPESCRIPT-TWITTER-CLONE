fc17a3d7d0ab39400a8ea7e0c86d3a2a70f4900b
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-29 12:08:13 +0600

    add follow functionality

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -4,11 +4,9 @@ import { ClipLoader } from 'react-spinners';
 import useUser from '@/hooks/useUser';
 import Header from '@/components/Header';
 
-// import PostFeed from '@/components/posts/PostFeed';
-// import Header from '@/components/Header';
-// import UserBio from '@/components/users/UserBio';
 import UserHero from '@/components/users/UserHero';
 import UserBio from '@/components/users/UserBio';
+import PostFeed from '@/components/posts/postFeed';
 
 const UserView = () => {
   const router = useRouter();
@@ -29,7 +27,7 @@ const UserView = () => {
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
-      {/* <PostFeed userId={userId as string} /> */}
+      <PostFeed userId={userId as string} />
     </>
   );
 };
